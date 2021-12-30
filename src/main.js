f1ce3e46fa2dd9e049e58cead03b5d7656469d5a
Xiaohui Kang <Xiaohui.Kang@luxshare-ict.com>
2021-12-30 18:39:26 +0800

    vue add vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@/style/index.css'
 import Notice from '@/utils/create.js'
+
 // 引入自定义路由插件
 import router from '@/myRouter'
+
+import store from './store'
 // import router from '@/router'
 
 Vue.use(Notice)
@@ -13,6 +16,8 @@ Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App),
+  store,
+
   beforeCreate() {
     Vue.prototype.$router1 = router
   }
